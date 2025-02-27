63dad3737a7703945dfbb0f5a731b8fb2c515f1a
HLBoun <154765503+HLBoun@users.noreply.github.com>
2025-02-26 19:04:05 -0600

    Create weatherDataManipulator.cpp

---
+++ weatherDataManipulator.cpp
@@ -0,0 +1,38 @@
+/*
+ * Filename: weatherDataManipulator.cpp
+ * Name: Huthsady Legend Boun
+ * Professor: Grace A. Comp
+ * Date: February 26, 2025
+ * Description: Program that accepts a year's worth of weather data and calculates averages, ranges, and avgs of avgs
+*/
+
+
+#include <iostream>
+#include <array>
+
+// Manually created include files
+#include "structs.h"
+
+
+
+
+
+
+struct Month
+{
+	std::string name;
+	double totalRainfall;
+
+	Month array[12];
+
+	array[0].name = "string";
+};
+
+
+
+
+
+// Main function, every cpp program needs it
+int main()
+{
+}
